397c9cc02b38b784d74d020f076194c284e13568
pfhaupt <pfhaupt@gmx.net>
2023-10-21 20:47:08 +0200

    Initial Commit
    - Add slides for first Rust session

---
+++ rust/example_exercise/src/main.rs
@@ -0,0 +1,9 @@
+// Rating 1/3 - Yellow
+fn main() {
+    let a = 0; // What type does `a` have?
+    let b = 5; // What type does `b` have?
+    let c: u32 = a + b;
+    println!("the v{a}lue of c is: {}", c);
+    // What's the output of the program?
+}
+
