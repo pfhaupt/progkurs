56267411c49e75ed1f2674220ccaf7131510d9ed
pfhaupt <pfhaupt@gmx.net>
2024-05-01 18:47:07 +0200

    [R-B-02] Update slides
    - Remove chapter about Type Inference
    - Better explain the Rust tools in Recap (cargo, rustc, rustup)
    - Make stuff look nicer

--- rust-beginner/02 - Variables/exercise_02_02/src/main.rs
+++ rust-beginner/02 - Variables/exercise_02_02/src/main.rs
@@ -1,13 +1,10 @@
-// Rating 2.5/3 - Reddish Purple
-// Topic: Types
-// Questions:
-// a) Does this compile?
-// b) What type does `a` have?
-// c) Does line 11 change the type of `a`?
+#![allow(unused)]
+
 fn main() {
     let a = 0;
     let b: i32 = a;
     let arr: [i32; 2] = [a, b];
     let d = a as usize;
     let e = arr[d];
+    println!("{}", e);
 } 
