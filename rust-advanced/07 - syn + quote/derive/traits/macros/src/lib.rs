36ad43785ffc2ede2e2156c2fa71bcf865f656bd
pfhaupt <pfhaupt@gmx.net>
2024-06-24 21:24:25 +0200

    [R-A-07] Cleanup code
    I am tired

--- rust-advanced/07 - syn + quote/derive/traits/macros/src/lib.rs
+++ rust-advanced/07 - syn + quote/derive/traits/macros/src/lib.rs
@@ -39,38 +39,3 @@ pub fn derive_type_info(input: TokenStream) -> TokenStream {
         }
     }.into()
 }
-/*
-    let DeriveInput { attrs, vis, ident, generics, data } = parse_macro_input!(input);
-    let typ = quote! { #ident #generics };
-    let Data::Struct(strukt) = data else { unimplemented!() };
-    let mut logged = Vec::new();
-    for field in &strukt.fields {
-        if let Some(attr) = field.attrs.get(0) {
-            let path = attr.path();
-            let ident = path.require_ident().unwrap_or_else(|e|panic!("{e}"));
-            let name = ident.to_string();
-            assert!(name == "log", "Only `log` is a valid attribute.");
-            logged.push(field.ident.as_ref().expect("Tupled structs are not supported yet."));
-        }
-    }
-    quote! {
-        impl #generics TypeInfo for #typ {
-            fn get_info() -> (usize, usize) {
-                #(println!("Logging field `{}`", stringify!(#logged)););*
-                ( std::mem::size_of::<#typ>(), std::mem::align_of::<#typ>() )
-            }
-        }
-    }.into()
- */
-// #[proc_macro_derive(TypeInfo)]
-// pub fn derive_type_info(input: TokenStream) -> TokenStream {
-//     let DeriveInput { ident, generics, ..} = parse_macro_input!(input);
-//     let typ = quote! { #ident #generics };
-//     quote! {
-//         impl #generics TypeInfo for #typ {
-//             fn get_info() -> (usize, usize) {
-//                 ( std::mem::size_of::<#typ>(), std::mem::align_of::<#typ>() )
-//             }
-//         }
-//     }.into()
-// }
