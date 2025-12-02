422598a550f7bee46c7b1f13b496234f1fe96d14
pfhaupt <pfhaupt@gmx.net>
2025-12-02 21:09:52 +0100

    [R-B-06] Update slides
    - Merge R-B-07 into R-B-06
    - Add chapter about Floating Point Numbers
    - Increase font size
    - Unite text font

--- rust-beginner/06 - Function declaration/float/src/main.rs
+++ rust-beginner/06 - Function declaration/float/src/main.rs
@@ -1,5 +1,19 @@
+use std::f64::consts::PI;
+
 fn main() {
-    let a: f32 = 0.0;
-    let b: f64 = 5.0 / 7.0;
-    let c: f32 = 0;
+    let a = 0.1;
+    let b = 0.2;
+    let c = a + b;
+    println!("a + b == {}", a + b);
+    println!("a + b == 0.3: {}", a + b == 0.3);
+    println!("    c == 0.3: {}", c == 0.3);
+    println!("c - b == a  : {}", c - b == a);
+    let approx = f64::abs((a + b) - c) < f64::EPSILON;
+    println!("close enough: {}", approx);
+
+    let mass = 5.972168e27; // g
+    let radius: f64 = 637_100_000.0; // cm
+    let volume = 4.0 / 3.0 * PI * radius.powi(3);
+    let density = mass / volume; // g/cm^3
+    println!("Earth's density: {:?}", density);
 }
